e4a87b533fa210e8da2ed7ae88ee37628355c944
luosenen <luosenen@gmail.com>
2018-10-16 19:27:08 +0800

    第七次更新源码，加入QQ互联，可以以QQ号登录分享

--- app/src/main/java/com/luosenen/huel/Core/Activity/LoginActivity.java
+++ app/src/main/java/com/luosenen/huel/Core/Activity/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -19,33 +20,55 @@ import android.widget.Toast;
 
 import com.luosenen.huel.Core.User.MyUser;
 import com.luosenen.huel.R;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.auth.QQToken;
+import com.tencent.connect.common.Constants;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.SaveListener;
 
+
 public class LoginActivity extends Activity implements View.OnClickListener {
 
-    private EditText edName,edPassword;
-    private Button btRegister,btLogin;
+    private EditText edName, edPassword;
+    private Button btRegister, btLogin, btQQ;
     private ProgressBar longinBar;
     private TextView app;
     private CheckBox box;
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
+    private static final String TAG = "LoginActivity";
+    private static final String APP_ID = "101508791";//官方获取的APPID
+    private Tencent mTencent;
+    private BaseUiListener mIUiListener;
+    private UserInfo mUserInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         MyUser userInfo = BmobUser.getCurrentUser(MyUser.class);
-        if (userInfo!=null){
-            Toast.makeText(getApplicationContext(),"登录成功：",Toast.LENGTH_LONG).show();
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            overridePendingTransition(R.anim.anim_in,R.anim.anim_out);
+        if (userInfo != null) {
+            Toast.makeText(getApplicationContext(), "登录成功：", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
             finish();
         }
+
+        mTencent = Tencent.createInstance(APP_ID,LoginActivity.this.getApplicationContext());
+        mIUiListener = new BaseUiListener();
+
         app = findViewById(R.id.appName);
         AssetManager assets = getAssets();
         Typeface fromAsset = Typeface.createFromAsset(assets, "font/font.ttf");
@@ -55,29 +78,31 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         longinBar = findViewById(R.id.loginIng);
         btRegister = findViewById(R.id.loginRegister);
         btLogin = findViewById(R.id.login);
+        btQQ = findViewById(R.id.loginQQ);
+        btQQ.setOnClickListener(this);
         btRegister.setOnClickListener(this);
         btLogin.setOnClickListener(this);
         box = findViewById(R.id.loginCheckBox);
-        preferences = getSharedPreferences("user",Context.MODE_PRIVATE);
+        preferences = getSharedPreferences("user", Context.MODE_PRIVATE);
         editor = preferences.edit();
-        edName.setText(preferences.getString("name",""));
-        edPassword.setText(preferences.getString("pass",""));
+        edName.setText(preferences.getString("name", ""));
+        edPassword.setText(preferences.getString("pass", ""));
 
     }
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.login:
                 longinBar.setVisibility(View.VISIBLE);
                 MyUser userInfo = BmobUser.getCurrentUser(MyUser.class);
-                if (userInfo!=null){
+                if (userInfo != null) {
 
-                    Toast.makeText(getApplicationContext(),"登录成功：",Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    Toast.makeText(getApplicationContext(), "登录成功：", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
                     finish();
-                }else {
+                } else {
                     final String name = edName.getText().toString().trim();
                     final String pass = edPassword.getText().toString().trim();
                     if (name.equals(null) || pass.equals(null)) {
@@ -102,7 +127,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                                 longinBar.setVisibility(View.INVISIBLE);
                                 Toast.makeText(getApplicationContext(), "登录成功：", Toast.LENGTH_LONG).show();
                                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                overridePendingTransition(R.anim.anim_in,R.anim.anim_out);
+                                overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
                                 finish();
                             } else {
                                 longinBar.setVisibility(View.INVISIBLE);
@@ -114,9 +139,99 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                 break;
 
             case R.id.loginRegister:
-                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
                 break;
+            case R.id.loginQQ:
+                mTencent.login(LoginActivity.this,"all", mIUiListener);
+
+
+                break;
+        }
+    }
+
+    private class BaseUiListener implements IUiListener {
+
+        Map<String ,String> map = new HashMap<>();
+        @Override
+        public void onComplete(Object response) {
+            Toast.makeText(LoginActivity.this, "授权成功", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "response:" + response);
+            JSONObject obj = (JSONObject) response;
+
+
+            try {
+                String openID = obj.getString("openid");
+                String accessToken = obj.getString("access_token");
+                String expires = obj.getString("expires_in");
+                map.put("user",openID);
+                MyUser user = new MyUser();
+
+                user.setUsername(openID);
+                user.setPassword(openID);
+                user.signUp(new SaveListener<MyUser>() {
+                    @Override
+                    public void done(MyUser myUser, BmobException e) {
+
+                    }
+                });
+                user.login(new SaveListener<MyUser>() {
+                    @Override
+                    public void done(MyUser myUser, BmobException e) {
+                        if (e==null){
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                    }
+                });
+                mTencent.setOpenId(openID);
+
+
+                mTencent.setAccessToken(accessToken,expires);
+                QQToken qqToken = mTencent.getQQToken();
+                mUserInfo = new UserInfo(getApplicationContext(),qqToken);
+                mUserInfo.getUserInfo(new IUiListener() {
+                    @Override
+                    public void onComplete(Object response) {
+
+
+                        Log.e(TAG,"登录成功"+response.toString());
+
+
+
+                    }
+                    @Override
+                    public void onError(UiError uiError) {
+                        Log.e(TAG,"登录失败"+uiError.toString());
+                    }
+                    @Override
+                    public void onCancel() {
+                        Log.e(TAG,"登录取消");
+                    }
+                });
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void onError(UiError uiError) {
+            Toast.makeText(LoginActivity.this, "授权失败", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onCancel() {
+            Toast.makeText(LoginActivity.this, "授权取消", Toast.LENGTH_SHORT).show();
+        }
+
+        public String ret(){
+
+            return map.get("user");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == Constants.REQUEST_LOGIN){
+            Tencent.onActivityResultData(requestCode,resultCode,data,mIUiListener);
 
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/luosenen/huel/Core/Activity/MySelfActivity.java
+++ app/src/main/java/com/luosenen/huel/Core/Activity/MySelfActivity.java
@@ -2,15 +2,70 @@ package com.luosenen.huel.Core.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.luosenen.huel.R;
+import com.tencent.connect.share.QQShare;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
 
 public class MySelfActivity extends Activity {
+    private Button sharQQ;
+
+    public static Tencent mTencent;
+    public static String mAppid="101508791";
+    private BaseUiListener listener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myself);
+        if (mTencent == null) {
+            mTencent = Tencent.createInstance(mAppid, this);
+            listener = new BaseUiListener();
+        }
+        sharQQ = findViewById(R.id.shareQQ);
+        sharQQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                qqShare(v);
+            }
+        });
 
 
+
+    }
+
+    public void qqShare(View view) {
+        Bundle params = new Bundle();
+        params.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+        params.putString(QQShare.SHARE_TO_QQ_TITLE, "云上财大");
+        params.putString(QQShare.SHARE_TO_QQ_SUMMARY,"Welcome To 河南财经政法大学\"云上财大\"社会学系技术支持部门");
+        params.putString(QQShare.SHARE_TO_QQ_TARGET_URL,"https://www.huel.site/");
+        params.putString(QQShare.SHARE_TO_QQ_IMAGE_URL,"https://p.qpic.cn/qqconnect/0/app_101508791_1539677114/100?max-age=2592000&t=0?362.3871387757305");
+        params.putString(QQShare.SHARE_TO_QQ_APP_NAME, "HUEL");
+        mTencent.shareToQQ(this, params, listener);
+    }
+
+    private class BaseUiListener implements IUiListener {
+
+        @Override
+        public void onComplete(Object o) {
+            Toast.makeText(getApplicationContext(), "分享成功！", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onError(UiError uiError) {
+
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
     }
 }
